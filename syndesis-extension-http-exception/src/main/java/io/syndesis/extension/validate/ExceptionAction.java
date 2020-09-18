89e11963d4219097b4453acb4507368c59436bdf
lucamaf <lucafe84@hotmail.com>
2020-09-18 12:23:04 +0200

    add delay field

--- syndesis-extension-http-exception/src/main/java/io/syndesis/extension/validate/ExceptionAction.java
+++ syndesis-extension-http-exception/src/main/java/io/syndesis/extension/validate/ExceptionAction.java
@@ -19,92 +19,122 @@ import org.apache.camel.Exchange;
 import org.apache.camel.model.ProcessorDefinition;
 import org.apache.camel.util.ObjectHelper;
 
-@Action(id = "Exception handling", name = "Exception", description = "Add a http exception catch step", tags = { "exception", "extension"})
+@Action(id = "Exception handling", name = "Exception", description = "Add a http exception catch step", tags = {
+    "exception",
+    "extension"
+})
 @ConfigurationProperties({
     @ConfigurationProperty(
         name = "endpoint",
         displayName = "Valid HTTP endpoint",
         description = "The HTTP endpoint called",
-        type = "String" ,
+        type = "String",
         required = true),
     @ConfigurationProperty(
         name = "method",
         displayName = "Valid HTTP method",
         description = "The HTTP method called",
-        type = "String" ,
+        type = "String",
         required = true),
     @ConfigurationProperty(
         name = "body",
         displayName = "Body to be sent with the request",
         description = "JSON Body to be forwarded to the endpoint",
-        type = "String" ,
+        type = "String",
+        required = false),
+    @ConfigurationProperty(
+        name = "delay",
+        displayName = "Acceptable delay waiting for the response",
+        description = "MS to wait before defaulting to standard behaviour",
+        type = "String",
         required = false)
 })
 public class ExceptionAction implements Step {
 
-   
+
     private String endpoint;
-    public String getEndpoint(){
+    public String getEndpoint() {
         return endpoint;
     }
-    public void setEndpoint(String endpoint){
+    public void setEndpoint(String endpoint) {
         this.endpoint = endpoint;
     }
 
-   
+
     private String method;
-    public String getMethod(){
+    public String getMethod() {
         return method;
     }
-    public void setMethod(String method){
+    public void setMethod(String method) {
         this.method = method;
     }
 
-    
+
     private String body;
-    public String getBody(){
+    public String getBody() {
         return body;
     }
-    public void setBody(String body){
+    public void setBody(String body) {
         this.body = body;
     }
 
-	@Override
-    public Optional<ProcessorDefinition<?>> configure(CamelContext context, ProcessorDefinition<?> route, Map<String, Object> parameters) {
+    private int delay;
+    public int getDelay() {
+        return delay;
+    }
+    public void setDelay(int delay) {
+        this.delay = delay;
+    }
+
+    @Override
+    public Optional < ProcessorDefinition < ? >> configure(CamelContext context, ProcessorDefinition < ? > route, Map <
+        String, Object > parameters) {
         ObjectHelper.notNull(route, "route");
         ObjectHelper.notNull(endpoint, "endpoint");
         ObjectHelper.notNull(method, "method");
-        
+
         return Optional.of(route.process((Exchange exchange) -> {
-            // my code
-            URL url = new URL(endpoint);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod(method.toUpperCase());
-            
-            con.setRequestProperty("Accept", "application/json");
-            con.setRequestProperty("Content-Type", "application/json");
-            //ApiCallResult apiCallResult = callRestAPI(endpoint);
+            try {
+                URL url = new URL(endpoint);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod(method.toUpperCase());
 
-            if(method.toUpperCase().equals("POST")){
-                con.setDoOutput(true);
-                try(OutputStream os = con.getOutputStream()) {
-                    byte[] input = body.getBytes("utf-8");
-                    os.write(input, 0, input.length);			
+                con.setRequestProperty("Accept", "application/json");
+                con.setRequestProperty("Content-Type", "application/json");
+                //ApiCallResult apiCallResult = callRestAPI(endpoint);
+                con.setConnectTimeout(delay); //set timeout to 5 seconds
+
+                if (method.toUpperCase().equals("POST")) {
+                    con.setDoOutput(true);
+                    try (OutputStream os = con.getOutputStream()) {
+                        byte[] input = body.getBytes("utf-8");
+                        os.write(input, 0, input.length);
+                    }
                 }
+                int statusCode = con.getResponseCode();
+                //String payload = con.getResponseMessage();
+                BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer payload = new StringBuffer();
+                while ((inputLine = in .readLine()) != null) {
+                    payload.append(inputLine);
+                } 
+                in .close();
+                exchange.getIn().setBody(payload);
+                exchange.getIn().setHeader("API_CALL_STATUS_CODE", statusCode);
+            } catch (java.net.SocketTimeoutException e) {
+                String payload = "{'reason':'delayed'}";
+                int statusCode = 202;
+                exchange.getIn().setBody(payload);
+                exchange.getIn().setHeader("API_CALL_STATUS_CODE", statusCode);
+            } catch (java.io.IOException e) {
+                String payload = "{'reason':'delayed'}";
+                int statusCode = 202;
+                exchange.getIn().setBody(payload);
+                exchange.getIn().setHeader("API_CALL_STATUS_CODE", statusCode);
             }
-            int statusCode = con.getResponseCode();
-            //String payload = con.getResponseMessage();
-            BufferedReader in = new BufferedReader(
-            new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer payload = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                payload.append(inputLine);
-            }
-            in.close();
 
-            exchange.getIn().setBody(payload);
-            exchange.getIn().setHeader("API_CALL_STATUS_CODE", statusCode);
         }));
     }
 }
